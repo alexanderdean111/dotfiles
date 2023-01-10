52d3134f5db30cc56f5bf0563178ebea515bc3cd
Alexander Dean <alexander.dean111@gmail.com>
2023-01-10 11:29:33 -0800

    lots of updates

--- setup.sh
+++ setup.sh
@@ -1,48 +1,37 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-EX_OK=0
-EX_ERR=1
-EX_USAGE=64
-
-ROOT_HOME="/root"
 ABS_PATH=$(readlink -f "$0")
 ABS_DIR=$(dirname "$ABS_PATH")
+DOTFILES_PATH="$HOME/dev/dotfiles"
 
-log()
-{
+log() {
   echo -e "\033[0;32m[+] $1\033[0m"
 }
 
-error_log()
-{
+error_log() {
   echo -e "\033[0;31m[-] $1\033[0m"
 }
 
-warning_log()
-{
+warning_log() {
   echo -e "\033[1;33m[-] $1\033[0m"
 
 }
 
-install_package()
-{
-  log "installing $1"
-  if which apt-get >/dev/null 2>&1; then
-    sudo http_proxy=$http_proxy https_proxy=$https_proxy apt install -y $1 
-  elif which dnf >/dev/null 2>&1; then
-    sudo http_proxy=$http_proxy https_proxy=$https_proxy dnf install -y $1
-  elif which yum >/dev/null 2>&1; then
-    sudo http_proxy=$http_proxy https_proxy=$https_proxy yum install -y $1
-  elif which pacman >/dev/null 2>&1; then
-    sudo http_proxy=$http_proxy https_proxy=$https_proxy pacman -S --noconfirm $1
+install_package() {
+  if ! command -v "$1" >/dev/null 2>&1; then
+    log "installing $1"
+    if which apt >/dev/null 2>&1; then
+      sudo http_proxy=$http_proxy https_proxy=$https_proxy apt install -y $1
+    else
+      error_log "not working on a debian based system, better do setup manually"
+      exit 1
+    fi
   else
-    error_log "package manager not found"
-    exit 1
+    warning_log "$1 already installed"
   fi
 }
 
-usage()
-{
+usage() {
   # Print usage message
   cat << EOF
 Usage $0 [options]
@@ -60,17 +49,39 @@ OPTIONS:
 EOF
 }
 
-ensure_sudo()
-{
-  user=$(id | cut -d'=' -f2 | cut -d\( -f1)
-  if [ $user -ne 0 ]; then
-    error_log "This option needs root authentication to install."
-    exit 1
-  fi
+setup_dotfiles() {
+  # setup dotfiles directory in ~/dev/ and copy SSH keys to ~/.ssh
+  install_package git
+  install_package gpg
+
+  # SSH keys need decrypted
+  log "decrypting SSH keys"
+  gpg --no-symkey-cache "$ABS_DIR/keys.tgz.gpg"
+  tar zxf "$ABS_DIR/keys.tgz" -C "$ABS_DIR"
+  rm -f "$ABS_DIR/keys.tgz"
+
+  # why isn't this a default directory
+  log "setting up SSH"
+  mkdir -p "$HOME/.ssh"
+
+  # move keys and fix permissions
+  mv -f "$ABS_DIR/id_ed25519_github" "$HOME/.ssh/id_ed25519_github"
+  mv -f "$ABS_DIR/id_ed25519_github.pub" "$HOME/.ssh/id_ed25519_github.pub"
+  chown -R "$(whoami):$(whoami)" "$HOME/.ssh"
+  chmod 700 "$HOME/.ssh"
+  chmod 600 "$HOME/.ssh/id_ed25519_github"
+  chmod 600 "$HOME/.ssh/id_ed25519_github.pub"
+
+  # clone the dotfiles directory into ~/dev/dotfiles
+  log "cloning dotfiles to $DOTFILES_PATH"
+  mkdir -p "$HOME/dev"
+  git clone git@github.com:alexanderdean111/dotfiles "$DOTFILES_PATH"
+  cd "$DOTFILES_PATH"
+  git config user.email "alexander.dean111@gmail.com"
+  git config user.name "Alexander Dean"
 }
 
-backup_config_file()
-{
+backup_config_file() {
   log "backing up $1"
   if [ -L $1 ]; then
     warning_log "Symlink $1 already exists, removing it"
@@ -88,22 +99,7 @@ backup_config_file()
   fi
 }
 
-clone_dotfiles()
-{
-  # Install git
-  if ! command -v git >/dev/null 2>&1; then
-    install_package git
-  fi
-
-  # Clone dotfiles
-  if [ ! -d "$ABS_DIR" ]; then
-    log "cloning dotfiles to $ABS_DIR"
-    git clone https://github.com/alexanderdean111/dotfiles.git $ABS_DIR
-  fi
-}
-
-setup_cron_updates()
-{
+setup_cron_updates() {
   # create a crontab file to install that will pull updates
   # from git every 6 hours
   log "Setting up cron job to update dotfiles every 6 hours"
@@ -112,18 +108,14 @@ setup_cron_updates()
   rm $ABS_DIR/crontab
 }
 
-install_zsh()
-{
+install_zsh() {
   # Install and configure ZSH. Can be used stand-alone.
-  if ! command -v zsh >/dev/null 2>&1; then
-    install_package zsh
-  fi
+  install_package zsh
 
   # link config
-  clone_dotfiles
   backup_config_file $HOME/.zshrc
   log "Creating symlink: $HOME/.zshrc"
-  ln -s "$ABS_DIR/zshrc" "$HOME/.zshrc"
+  ln -s "$DOTFILES_PATH/zshrc" "$HOME/.zshrc"
 
   # Grab general ZSH config via oh-my-zsh project
   # See https://github.com/robbyrussell/oh-my-zsh
@@ -136,47 +128,39 @@ install_zsh()
   # Set ZSH as my default shell
   backup_config_file $HOME/.bash_profile
   log "Creating symlink: $HOME/.bash_profile"
-  ln -s "$ABS_DIR/bash_profile" "$HOME/.bash_profile"
+  ln -s "$DOTFILES_PATH/bash_profile" "$HOME/.bash_profile"
 
   # Launch zsh at the end of .bashrc
   echo -e "$(which zsh)" >> "$HOME/.bashrc"
 }
 
-install_tmux()
-{
-  if ! command -v tmux >/dev/null 2>&1; then
-    install_package tmux
-  fi
-
-  if ! command -v xclip >/dev/null 2>&1; then
-    install_package xclip
-  fi
+install_tmux() {
+  install_package tmux
+  install_package xclip
 
   # link config
-  clone_dotfiles
   backup_config_file $HOME/.tmux.conf
   log "Creating symlink: $HOME/.tmux.conf"
-  ln -s "$ABS_DIR/tmux.conf" "$HOME/.tmux.conf"
+  ln -s "$DOTFILES_PATH/tmux.conf" "$HOME/.tmux.conf"
 }
 
-install_vim()
-{
+install_vim() {
 
   log "installing vim plugins"
   ret=$(git clone https://github.com/vim-syntastic/syntastic.git \
-    $ABS_DIR/vim/pack/vendor/start/syntastic 2>&1)
+    $DOTFILES_PATH/vim/pack/vendor/start/syntastic 2>&1)
   if [[ $ret =~ "already exists" ]]; then
     warning_log "Syntastic already installed, skipping"
   fi
 
   ret=$(git clone https://github.com/nvie/vim-flake8.git \
-    $ABS_DIR/vim/pack/vendor/start/vim-flake8 2>&1)
+    $DOTFILES_PATH/vim/pack/vendor/start/vim-flake8 2>&1)
   if [[ $ret =~ "already exists" ]]; then
     warning_log "vim-flake8 already installed, skipping"
   fi
   
   ret=$(git clone https://github.com/preservim/nerdtree.git \
-    $ABS_DIR/vim/pack/vendor/start/nerdtree 2>&1)
+    $DOTFILES_PATH/vim/pack/vendor/start/nerdtree 2>&1)
   if [[ $ret =~ "already exists" ]]; then
     warning_log "nerdtree already installed, skipping"
   fi
@@ -184,32 +168,17 @@ install_vim()
   # link vim modules config
   backup_config_file $HOME/.vim
   log "Creating symlink: $HOME/.vim"
-  ln -s "$ABS_DIR/vim" "$HOME/.vim"
+  ln -s "$DOTFILES_PATH/vim" "$HOME/.vim"
 
-  if ! command -v vim >/dev/null 2>&1; then
-    install_package vim-minimal
-    install_package vim-X11
-    install_package vim
-    install_package vim-enhanced
-  fi
+  install_package vim
 
   # link config
-  clone_dotfiles
   backup_config_file $HOME/.vimrc
   log "Creating symlink: $HOME/.vimrc"
-  ln -s "$ABS_DIR/vimrc" "$HOME/.vimrc"
+  ln -s "$DOTFILES_PATH/vimrc" "$HOME/.vimrc"
 }
 
-install_git()
-{
-  clone_dotfiles
-  backup_config_file $HOME/.gitconfig
-  log "Creating symlink: $HOME/.gitconfig"
-  ln -s $ABS_DIR/gitconfig $HOME/.gitconfig
-}
-
-misc_software()
-{
+misc_software() {
   install_package python3
   install_package python3-dev
   install_package python3-venv
@@ -221,37 +190,38 @@ misc_software()
   install_package xclip
 
   log "deleting stupid default directories"
-  rmdir ~/Templates
-  rmdir ~/Music
-  rmdir ~/Public
-  rmdir ~/Videos
+  rmdir ~/Templates >/dev/null 2>&1
+  rmdir ~/Music >/dev/null 2>&1
+  rmdir ~/Public >/dev/null 2>&1
+  rmdir ~/Videos >/dev/null 2>&1
 }
 
 # If executed with no options
 if [ $# -eq 0 ]; then
   usage
-  exit $EX_USAGE
+  exit 1
 fi
+
 
 while getopts ":hacztvus" opt; do
   case "$opt" in
     h)
       # Help message
       usage
-      exit $EX_OK
+      exit 1
       ;;
     a)
-      # Do everything except install i3
-      clone_dotfiles
+      # Do everything
+      setup_dotfiles
       misc_software
-      setup_cron_updates
       install_zsh
       install_tmux
       install_vim
+      setup_cron_updates
       ;;
     c)
       # Clone configuration files
-      clone_dotfiles
+      setup_dotfiles
       ;;
     z)
       # Install and setup ZSH shell
@@ -276,6 +246,6 @@ while getopts ":hacztvus" opt; do
     *)
       # All other flags fall through to here
       usage
-      exit $EX_USAGE
+      exit 1
   esac
 done
